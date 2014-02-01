3cbd566253d78ee9c7bb74f4762d0bb1cdf49d7b
Adam Syed <suedadam@gmail.com>
2014-01-31 21:26:14 -0800

    adding strong.py

---
+++ strong.py
@@ -0,0 +1,39 @@
+from os import urandom
+from random import choice
+
+char_set = {'small': 'abcdefghijklmnopqrstuvwxyz',
+             'nums': '0123456789',
+             'big': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+             'special': '^!\$%&/()=?{[]}+~#-_.:,;<>|\\'
+            }
+
+
+def generate_pass(length=24):
+
+    password = []
+
+    while len(password) < length:
+        key = choice(char_set.keys())
+        a_char = urandom(1)
+        if a_char in char_set[key]:
+            if check_prev_char(password, char_set[key]):
+                continue
+            else:
+                password.append(a_char)
+    return ''.join(password)
+
+
+def check_prev_char(password, current_char_set):
+
+    index = len(password)
+    if index == 0:
+        return False
+    else:
+        prev_char = password[index - 1]
+        if prev_char in current_char_set:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    print generate_pass()
